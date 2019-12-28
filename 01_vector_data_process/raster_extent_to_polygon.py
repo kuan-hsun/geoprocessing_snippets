eea27ecb42edef0dcea9685e2ebc5c48c81845c3
Kuan-Hsun Cho <33713306+kuan-hsun@users.noreply.github.com>
2019-12-28 11:08:58 +0800

    Update script raster_extent_to_polygon

--- 01_vector_data_process/raster_extent_to_polygon.py
+++ 01_vector_data_process/raster_extent_to_polygon.py
@@ -1,10 +1,10 @@
-raster = arcpy.sa.Raster('高鐵旗山斷層_ps_S1d_defo_48期_mean_v')
+raster = arcpy.sa.Raster('your_raster_file')
 thisExtent = raster.extent
 xmax = thisExtent.XMax 
 ymax = thisExtent.YMax
 xmin = thisExtent.XMin
 ymin = thisExtent.YMin
-fc = '高鐵_旗山斷層_extent'
+fc = 'your_raster_file_extent'
 cursor = arcpy.da.InsertCursor(fc, ["SHAPE@"])
 array1 = arcpy.Array([arcpy.Point(xmin, ymax),arcpy.Point(xmax, ymax),arcpy.Point(xmax, ymin),arcpy.Point(xmin, ymin),arcpy.Point(xmin, ymax)])
 polygon1 = arcpy.Polygon(array1)
